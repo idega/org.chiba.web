142b909c82a8bd87bd2c78035d30507cd7c8e375
Arunas <arunas@idega.com>
2008-03-26 07:44:20 +0000

    *** empty log message ***

--- src/java/com/idega/chiba/web/xml/xforms/connector/webdav/FileUploadManager.java
+++ src/java/com/idega/chiba/web/xml/xforms/connector/webdav/FileUploadManager.java
@@ -1,14 +1,15 @@
 package com.idega.chiba.web.xml.xforms.connector.webdav;
 
+import java.io.File;
 import java.util.List;
 
 import org.w3c.dom.Node;
 
 /**
  * @author <a href="mailto:arunas@idega.com">Arūnas Vasmanas</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  *
- * Last modified: $Date: 2008/03/19 11:40:19 $ by $Author: arunas $
+ * Last modified: $Date: 2008/03/26 07:44:35 $ by $Author: arunas $
  */
 
 public abstract class FileUploadManager {
@@ -16,6 +17,6 @@ public abstract class FileUploadManager {
   /*  public FileUploadManager(Node instance) {	
     }
 */    
-    public abstract List<FileItem> getFiles(String identifier, Node instance) ;
+    public abstract List<File> getFiles(String identifier, Node instance) ;
 
 }
--- src/java/com/idega/chiba/web/xml/xforms/connector/webdav/FileUploads.java
+++ src/java/com/idega/chiba/web/xml/xforms/connector/webdav/FileUploads.java
@@ -1,34 +1,36 @@
 package com.idega.chiba.web.xml.xforms.connector.webdav;
 
 import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+
 import javax.faces.context.FacesContext;
 
-import com.idega.util.CoreConstants;
-import com.idega.util.StringUtil;
 import org.chiba.web.IWBundleStarter;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import com.idega.idegaweb.IWMainApplication;
+import com.idega.util.CoreConstants;
 import com.idega.util.FileUtil;
+import com.idega.util.StringUtil;
 import com.idega.util.xml.XPathUtil;
 
 /**
  * @author <a href="mailto:arunas@idega.com">Arūnas Vasmanas</a>
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  *
- * Last modified: $Date: 2008/03/20 13:01:36 $ by $Author: arunas $
+ * Last modified: $Date: 2008/03/26 07:44:20 $ by $Author: arunas $
  */
  
 public class FileUploads extends FileUploadManager {
 
     public static final String UPLOADS_PATH = IWMainApplication.getIWMainApplication(FacesContext.getCurrentInstance()).getBundle(IWBundleStarter.BUNDLE_IDENTIFIER).getBundleBaseRealPath() + "/uploads/";
     private static final String ENTRIES = "./child::entry";
-    private static final String FILENAME = "filename";
     private static final String VARIABLE_ELEMENT = "./descendant::node()[name(.) = $elementName]";
     private static final String ELEMENT_NAME = "elementName";
     private static final String VARIABLE = ".//entry[@filename]";
@@ -44,26 +46,25 @@ public class FileUploads extends FileUploadManager {
 
     }
 
-    public List<FileItem> getFiles(String identifier, Node instance) {
+    public List<File> getFiles(String identifier, Node instance) {
 
-	List<FileItem> fileList = new ArrayList<FileItem>();
+	List<File> fileList = new ArrayList<File>();
 
 	Element node = getUploadsElement(identifier, instance);
-
+	
 	XPathUtil util = new XPathUtil(ENTRIES);
 	NodeList entries = util.getNodeset(node);
 
 	for (int i = 0; i < entries.getLength(); i++) {
-
-	    Element entryElment = (Element) entries.item(i);
-
-	    List<String> valuesFromString = StringUtil.getValuesFromString(entryElment.getTextContent(), CoreConstants.SLASH);
-	    int listSize = valuesFromString.size();
-	    String path = UPLOADS_PATH + valuesFromString.get(listSize - 2) + CoreConstants.SLASH + valuesFromString.get(listSize - 1);
-
-	    File file = new File(path);
-	    FileItem fileItems = new FileItem(entryElment.getAttribute(FILENAME), file);
-	    fileList.add(fileItems);
+	
+	    try {
+		   URI uri = new URI (entries.item(i).getTextContent());
+		   File file = new File(uri);
+		   fileList.add(file);
+	    } catch (URISyntaxException e) {
+		 e.printStackTrace();
+	    }
+	 
 	}
 	
 	return fileList;
@@ -84,7 +85,8 @@ public class FileUploads extends FileUploadManager {
 
 	HashSet<String> filesFolders = new HashSet<String>();
 	List<String> valuesFromString = StringUtil.getValuesFromString(entry.getTextContent(), CoreConstants.SLASH);
-	filesFolders.add(valuesFromString.get(valuesFromString.size() - 2));
+	List<String> pathValues= StringUtil.getValuesFromString(UPLOADS_PATH, CoreConstants.SLASH);
+	filesFolders.add(valuesFromString.get(pathValues.size()));
 
 	return filesFolders;
 
