0d4e65062168e2d940866a8fa3573685ba5d5917
Arunas <arunas@idega.com>
2008-04-02 12:09:08 +0000

    *** empty log message ***

---
+++ src/java/com/idega/chiba/web/xml/xforms/connector/webdav/FileUploadDaemon.java
@@ -0,0 +1,49 @@
+package com.idega.chiba.web.xml.xforms.connector.webdav;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import com.idega.util.EventTimer;
+import com.idega.util.FileUtil;
+import com.idega.util.IWTimestamp;
+
+/**
+ * @author <a href="mailto:civilis@idega.com">Arūnas Vasmanas</a>
+ * @version $Revision: 1.1 $
+ *
+ * Last modified: $Date: 2008/04/02 12:09:08 $ by $Author: arunas $
+ */
+
+public class FileUploadDaemon implements ActionListener {
+
+    public static final String THREAD_NAME= "uploaded_file_Daemon";
+    private EventTimer fileTimer;
+
+    public void start() {
+	// checking uploaded files every hour. 
+	this.fileTimer = new EventTimer(EventTimer.THREAD_SLEEP_1_HOUR,THREAD_NAME);
+	this.fileTimer.addActionListener(this);
+	// Starts the thread after 5 mins.
+	this.fileTimer.start(EventTimer.THREAD_SLEEP_5_MINUTES);
+    }
+
+    public void actionPerformed(ActionEvent event) {
+	try {
+	    if (event.getActionCommand().equalsIgnoreCase(THREAD_NAME)) {
+		System.out.println("[File Upload Daemon - "+ IWTimestamp.RightNow().toString()	+ "] - Deleting uploaded files older than 24h");
+		// deleting uploaded files older than 24h.
+		FileUtil.deleteAllFilesAndFolderInFolderOlderThan(FileUploadManager.UPLOADS_PATH, fileTimer.THREAD_SLEEP_24_HOURS);
+	    }
+
+	} catch (Exception x) {
+	    x.printStackTrace();
+	}
+    }
+
+    public void stop() {
+	if (this.fileTimer != null) {
+	    this.fileTimer.stop();
+	    this.fileTimer = null;
+	}
+
+    }
+}
