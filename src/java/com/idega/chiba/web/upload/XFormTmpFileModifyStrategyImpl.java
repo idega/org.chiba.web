4a0f1682a875969ad7f304c11c09bd9f60fc538f
Vytautas Civilis <civilis@idega.com>
2008-06-28 19:13:43 +0000

    new

---
+++ src/java/com/idega/chiba/web/upload/XFormTmpFileModifyStrategyImpl.java
@@ -0,0 +1,141 @@
+package com.idega.chiba.web.upload;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.net.URI;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+import org.w3c.dom.Node;
+
+import com.idega.business.IBOLookup;
+import com.idega.business.IBOLookupException;
+import com.idega.core.file.tmp.TmpFileResolverType;
+import com.idega.core.file.tmp.TmpFilesModifyStrategy;
+import com.idega.core.file.util.FileInfo;
+import com.idega.core.file.util.FileURIHandler;
+import com.idega.core.file.util.FileURIHandlerFactory;
+import com.idega.core.test.base.IdegaBaseTest;
+import com.idega.idegaweb.IWApplicationContext;
+import com.idega.idegaweb.IWMainApplication;
+import com.idega.presentation.IWContext;
+import com.idega.slide.business.IWSlideService;
+import com.idega.util.CoreConstants;
+
+
+/**
+ * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
+ * @version $Revision: 1.1 $
+ *
+ * Last modified: $Date: 2008/06/28 19:13:43 $ by $Author: civilis $
+ */
+@Scope("singleton")
+@TmpFileResolverType("xformSlide")
+@Service
+public class XFormTmpFileModifyStrategyImpl implements TmpFilesModifyStrategy {
+	
+	static final String SLIDE_STORE_PATH = "/files/forms/saved/uploadedFiles/";
+	static final String SLIDE_SCHEME = "slide";
+	
+	@Autowired
+	private FileURIHandlerFactory fileURIHandlerFactory;
+
+	public URI executeMove(URI uri, Object resource) {
+
+		if(!(resource instanceof Node))
+			throw new IllegalArgumentException("Wrong type resource argument provided = "+resource.getClass().getName()+" instead of "+Node.class.getName());
+		
+		Node item = (Node)resource;
+		
+		FileURIHandler furihandler = getFileURIHandlerFactory().getHandler(uri);
+		InputStream fis = null;
+		URI newUri = null;
+		
+		try {
+			FileInfo finfo = furihandler.getFileInfo(uri);
+			
+//			trying to resolve parent folder, and reuse that
+			String path = uri.getPath();
+			
+			String parentFolder = resolveParentFolder(path);
+			
+			if(parentFolder == null)
+				parentFolder = String.valueOf(System.currentTimeMillis());
+
+//			building slide store path
+			String folder = SLIDE_STORE_PATH+parentFolder;
+			newUri = new URI(SLIDE_SCHEME, folder+CoreConstants.SLASH+finfo.getFileName(), null);
+			
+			item.setTextContent(newUri.toString());
+			
+			if(System.getProperty(IdegaBaseTest.testSystemProp) == null) {
+				
+				fis = furihandler.getFile(uri);
+
+				IWSlideService slideService = getIWSlideService();
+				slideService.uploadFileAndCreateFoldersFromStringAsRoot(folder, finfo.getFileName(), fis, null, false);
+			}
+			
+		} catch (FileNotFoundException e) {
+			Logger.getLogger(getClass().getName()).log(Level.SEVERE, "Exception while resolving file by uri="+uri+", skipping", e);
+			newUri = null;
+		} catch (Exception e) {
+			Logger.getLogger(getClass().getName()).log(Level.SEVERE, "Exception while moving file resource from uri="+uri+" to uri="+newUri, e);
+			newUri = null;
+		} finally {
+			
+			if(fis != null)
+				try { fis.close(); } catch (Exception e) { Logger.getLogger(getClass().getName()).log(Level.WARNING, "Exception closing input stream", e);}
+		}
+		
+		return newUri;
+	}
+	
+	private String resolveParentFolder(String path) {
+		
+		String parentFolder = null;
+		
+		if(path.contains(CoreConstants.SLASH)) {
+			
+			String[] vals = path.split(CoreConstants.SLASH);
+			
+			if(vals.length > 1) {
+				
+				parentFolder = vals[vals.length-2];
+			}
+		}
+		
+		return !CoreConstants.EMPTY.equals(parentFolder) ? parentFolder : null;
+	}
+
+	public FileURIHandlerFactory getFileURIHandlerFactory() {
+		return fileURIHandlerFactory;
+	}
+	
+	private IWSlideService getIWSlideService() throws IBOLookupException {
+		
+		try {
+			return (IWSlideService) IBOLookup.getServiceInstance(getIWApplicationContext(), IWSlideService.class);
+		} catch (IBOLookupException e) {
+			Logger.getLogger(getClass().getName()).log(Level.SEVERE, "Error getting IWSlideService");
+			throw e;
+		}
+	}
+	
+	private IWApplicationContext getIWApplicationContext() {
+		
+		final IWContext iwc = IWContext.getCurrentInstance();
+		
+		final IWApplicationContext iwac;
+		
+		if(iwc != null)
+			iwac = iwc;
+		else
+			iwac = IWMainApplication.getDefaultIWApplicationContext();
+		
+	    return iwac;
+	}
+}
---
+++ src/test/java/com/idega/chiba/web/upload/XFormTmpFileResolverImplTest.java
@@ -0,0 +1,73 @@
+package com.idega.chiba.web.upload;
+
+import java.io.InputStream;
+import java.net.URI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+import com.idega.core.file.tmp.TmpFileResolver;
+import com.idega.core.file.tmp.TmpFileResolverType;
+import com.idega.core.file.tmp.TmpFilesModifyStrategy;
+import com.idega.core.test.base.IdegaBaseTest;
+import com.idega.util.xml.XPathUtil;
+import com.idega.util.xml.XmlUtil;
+
+
+/**
+ * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
+ * @version $Revision: 1.1 $
+ *
+ * Last modified: $Date: 2008/06/28 19:13:59 $ by $Author: civilis $
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+public final class XFormTmpFileResolverImplTest extends IdegaBaseTest {
+
+	@Autowired
+	@TmpFileResolverType("xformVariables")
+	private TmpFileResolver tmpFileResolver;
+	@Autowired
+	@TmpFileResolverType("xformSlide")
+	private TmpFilesModifyStrategy tmpFilesModifyStrategy;
+	
+	@Test
+	public void testResolveAllFilesUris() throws Exception {
+		
+		System.setProperty(IdegaBaseTest.testSystemProp, "true");
+		ClassPathResource cpr = new ClassPathResource("submissionInstance2AttachmentsStart.xml", getClass());
+		URI startSubmissionUri = cpr.getURI();
+		InputStream is = cpr.getInputStream();
+		cpr = new ClassPathResource("XFormTmpFileResolverImplTest-context.xml", getClass());
+		URI f2uri = cpr.getURI();
+		final Document submissionInstance;
+		
+		try {
+			submissionInstance = XmlUtil.getDocumentBuilder().parse(is);
+			
+		} finally {
+			
+			if(is != null)
+				is.close();
+		}
+		
+		XPathUtil entryFilesXPath = new XPathUtil(".//entry[@filename]");
+		NodeList filesNodes = entryFilesXPath.getNodeset(submissionInstance);
+//		expecting 2 items
+		filesNodes.item(0).setTextContent(startSubmissionUri.toString());
+		filesNodes.item(1).setTextContent(f2uri.toString());
+		
+		tmpFileResolver.replaceAllFiles(submissionInstance, tmpFilesModifyStrategy);
+		
+		filesNodes = entryFilesXPath.getNodeset(submissionInstance);
+//		expecting 2 items
+		assertEquals("slide:/files/forms/saved/uploadedFiles/upload/submissionInstance2AttachmentsStart.xml", filesNodes.item(0).getTextContent());
+		assertEquals("slide:/files/forms/saved/uploadedFiles/upload/XFormTmpFileResolverImplTest-context.xml", filesNodes.item(1).getTextContent());
+	}
+}
