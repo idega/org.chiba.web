42948a51305c72d628242a4ee7795d79f19fedd8
Arunas <arunas@idega.com>
2008-09-22 13:51:08 +0000

    *** empty log message ***

--- src/java/com/idega/chiba/web/xml/xforms/functions/IdegaExtensionFunctions.java
+++ src/java/com/idega/chiba/web/xml/xforms/functions/IdegaExtensionFunctions.java
@@ -1,12 +1,24 @@
 package com.idega.chiba.web.xml.xforms.functions;
 
+import java.util.Iterator;
+
 import com.idega.core.accesscontrol.business.LoginDBHandler;
 
+
+import org.apache.commons.jxpath.ExpressionContext;
+import org.apache.commons.jxpath.Pointer;
+import org.chiba.xml.dom.DOMUtil;
+import org.chiba.xml.xforms.Container;
+import org.chiba.xml.xforms.core.Instance;
+import org.chiba.xml.xforms.core.ModelItem;
+import org.chiba.xml.xforms.core.Validator;
+import org.chiba.xml.xforms.xpath.ExtensionFunctionsHelper;
+import org.apache.commons.jxpath.JXPathContext;
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  *
- * Last modified: $Date: 2008/09/19 11:54:00 $ by $Author: civilis $
+ * Last modified: $Date: 2008/09/22 13:51:08 $ by $Author: arunas $
  */
 public class IdegaExtensionFunctions {
 
@@ -19,4 +31,39 @@ public class IdegaExtensionFunctions {
     	
     	return LoginDBHandler.isLoginInUse(userName);
     }
+    
+    public static boolean validate (ExpressionContext expressionContext, String idref){
+    	  // get chiba container
+        Container container = ExtensionFunctionsHelper.getChibaContainer(expressionContext);
+        if (container == null) {
+            return false;
+        }
+
+        // lookup instance object
+        Object object = container.lookup(idref);
+        if (object != null && object instanceof Instance) {
+            // get root element node pointer
+        	Instance instance = (Instance) object;
+
+        	Pointer point = ((Instance) object).getPointer(org.chiba.xml.xpath.XPathUtil.OUTERMOST_CONTEXT);
+              
+			System.out.println(point.toString());
+
+        	Iterator<?> iterator = instance.iterateModelItems(org.chiba.xml.xpath.XPathUtil.OUTERMOST_CONTEXT);
+            ModelItem modelItem;
+            while (iterator.hasNext()) {
+                modelItem = (ModelItem) iterator.next();
+                System.out.println("items=" +modelItem.getValue());
+            }
+        	
+                  	DOMUtil.prettyPrintDOM(instance.getElement());
+        	 
+            return false;
+        }
+
+        return false;
+
+    }
+    
+   
 }
