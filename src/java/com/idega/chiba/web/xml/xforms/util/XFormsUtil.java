af898628a96744a6228373a089e4497624c8d5ab
Vytautas Civilis <civilis@idega.com>
2008-10-06 11:21:23 +0000

    getValueFromExpression(String valueAttribute, AbstractFormControl formControl) added

--- src/java/com/idega/chiba/web/xml/xforms/util/XFormsUtil.java
+++ src/java/com/idega/chiba/web/xml/xforms/util/XFormsUtil.java
@@ -9,15 +9,16 @@ import org.chiba.xml.xforms.core.BindingResolver;
 import org.chiba.xml.xforms.core.Instance;
 import org.chiba.xml.xforms.exception.XFormsComputeException;
 import org.chiba.xml.xforms.exception.XFormsException;
+import org.chiba.xml.xforms.ui.AbstractFormControl;
 import org.chiba.xml.xforms.ui.BindingElement;
 import org.w3c.dom.Node;
 
 /**
  * 
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  *
- * Last modified: $Date: 2008/09/30 20:28:17 $ by $Author: civilis $
+ * Last modified: $Date: 2008/10/06 11:21:23 $ by $Author: civilis $
  *
  */
 public class XFormsUtil {
@@ -51,6 +52,44 @@ public class XFormsUtil {
         }
         catch (Exception e) {
             throw new XFormsComputeException("invalid value expression at " + action, e, action.getTarget(), valueAttribute);
+        }
+        Object value = context.getValue("chiba:undeclare('output-value')");
+        context.getVariables().undeclareVariable("currentContextPath");
+        context.getVariables().undeclareVariable("contextmodel");
+
+        // check for string conversion to prevent sth. like "5 + 0" to be evaluated to "5.0"
+        if (value instanceof Double) {
+            // additionaly check for special cases
+            double doubleValue = ((Double) value).doubleValue();
+            if (!(Double.isNaN(doubleValue) || Double.isInfinite(doubleValue))) {
+                value = context.getValue("string(" + value + ")");
+            }
+        }
+
+        return value;
+    }
+	
+	public static Object getValueFromExpression(String valueAttribute, AbstractFormControl formControl) throws XFormsException {
+        String pathExpression = BindingResolver.getExpressionPath(formControl, formControl.getRepeatItemId());
+        Instance instance = formControl.getModel().getInstance(formControl.getModel().computeInstanceId(pathExpression));
+        if (!instance.existsNode(pathExpression)) {
+            return null;
+        }
+
+        // todo: implement XPathProcessor abstraction, this code is copied from SetValueAction
+        // since jxpath doesn't provide a means for evaluating an expression
+        // in a certain context, we use a trick here: the expression will be
+        // evaluated during getPointer and the result stored as a variable
+        JXPathContext context = instance.getInstanceContext();
+
+        String currentPath = getParentContextPath(formControl.getElement());
+        context.getVariables().declareVariable("currentContextPath", currentPath);
+        context.getVariables().declareVariable("contextmodel", formControl.getModel().getId());
+        try {
+            context.getPointer(pathExpression + "[chiba:declare('output-value', " + valueAttribute + ")]");
+        }
+        catch (Exception e) {
+        	throw new XFormsComputeException("invalid value expression at " + formControl, e, formControl.getTarget(), valueAttribute);
         }
         Object value = context.getValue("chiba:undeclare('output-value')");
         context.getVariables().undeclareVariable("currentContextPath");
