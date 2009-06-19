32ecb45d0c090a7a91d91ca5d67bcf7746417297
Valdas Zemaitis <valdas@idega.com>
2009-06-19 11:28:36 +0000

    Localization for Icelandic

---
+++ resources/javascript/dojo-0.4.3/src/i18n/calendar/nls/is_is/gregorian.js
@@ -0,0 +1,20 @@
+({
+	'months-format-wide': ["Janúar", "Febrúar", "Mars", "Apríl", "Maí", "Júní", "Júlí", "Ágúst", "September", "Október", "Nóvember", "Desember"],
+	'months-format-abbr': ["Jan", "Feb", "Mar", "Apr", "Maí", "Jún", "Júl", "Ágú", "Sep", "Okt", "Nóv", "Des"],
+	'months-standAlone-narrow': ["J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D"],
+	'days-format-wide': ["Sunnudagur", "Mánudagur", "Þriðjudagur", "Miðvikudagur", "Fimmtudagur", "Föstudagur", "Laugardagur"],
+	'days-format-abbr': ["Sun", "Mán", "Þri", "Mið", "Fim", "Fös", "Lau"],
+	'days-standAlone-narrow': ["S", "M", "T", "M", "F", "F", "L"],
+
+	'dateFormat-full': "EEEE, MMMM d, yyyy",
+	'dateFormat-long': "MMMM d, yyyy",
+	'dateFormat-medium': "MMM d, yyyy",
+	'dateFormat-short': "d.M.yy",
+
+	'timeFormat-full': "h:mm:ss a v",
+	'timeFormat-long': "h:mm:ss a z",
+	'timeFormat-medium': "h:mm:ss a",
+	'timeFormat-short': "h:mm a",
+
+	eras: ['BC','AD']
+})
