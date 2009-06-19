a285f83b56e4c1ddc6687f1bc6e2f3af94090473
Valdas Zemaitis <valdas@idega.com>
2009-06-19 11:27:17 +0000

    Setting init info

--- resources/javascript/xformsConfig.js
+++ resources/javascript/xformsConfig.js
@@ -1,9 +1,17 @@
+var XFormsConfig = {};
+
 if (djConfig == null) {	
 	var djConfig = {
 		baseScriptUri:	'/idegaweb/bundles/org.chiba.web.bundle/resources/javascript/dojo-0.4.3/',
 		debugAtAllCost:	false,
 		isDebug:		false
 	};
+}
+
+XFormsConfig.setConfiguration = function(config) {
+	djConfig.baseScriptUri = config.baseScriptUri;
+	djConfig.locale = config.locale;
+	dojo.locale = config.locale;
 }
 
 jQuery(window).load(function() {
