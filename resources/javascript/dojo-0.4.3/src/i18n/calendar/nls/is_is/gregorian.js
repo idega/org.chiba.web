1a03bab0c04f1ea2f7adb367980d30fc8f0e68ee
Valdas Zemaitis <valdas@idega.com>
2009-06-19 11:34:20 +0000

    *** empty log message ***

--- resources/javascript/dojo-0.4.3/src/i18n/calendar/nls/is_is/gregorian.js
+++ resources/javascript/dojo-0.4.3/src/i18n/calendar/nls/is_is/gregorian.js
@@ -4,7 +4,7 @@
 	'months-standAlone-narrow': ["J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D"],
 	'days-format-wide': ["Sunnudagur", "Mánudagur", "Þriðjudagur", "Miðvikudagur", "Fimmtudagur", "Föstudagur", "Laugardagur"],
 	'days-format-abbr': ["Sun", "Mán", "Þri", "Mið", "Fim", "Fös", "Lau"],
-	'days-standAlone-narrow': ["S", "M", "T", "M", "F", "F", "L"],
+	'days-standAlone-narrow': ["S", "M", "Þ", "M", "F", "F", "L"],
 
 	'dateFormat-full': "EEEE, MMMM d, yyyy",
 	'dateFormat-long': "MMMM d, yyyy",
