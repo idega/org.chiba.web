699a7384d28ed2c5739e8ad120d72a19c2db362f
Valdas Žemaitis <valdas@idega.com>
2009-08-25 17:44:06 +0300

    Enabled full screen mode, disabled resizing

--- resources/javascript/FluxInterface.js
+++ resources/javascript/FluxInterface.js
@@ -1,3 +1,4 @@
+
 /******************************************************************************
  GENERAL STUFF
  ******************************************************************************/
@@ -613,14 +614,14 @@ FluxInterfaceHelper.initializeTinyMCE = function() {
 				
 				language: languageId,
 				
-				theme_advanced_buttons1: 'styleselect,formatselect,fontselect,fontsizeselect',
+				theme_advanced_buttons1: 'fullscreen,|,styleselect,formatselect,fontselect,fontsizeselect',
 				theme_advanced_buttons2: 'bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,link,unlink,image,|,insertdate,inserttime,preview,|,forecolor,backcolor',
 				theme_advanced_buttons3: 'cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo',
 				theme_advanced_buttons4: 'tablecontrols,|,hr,removeformat,visualaid,|,charmap,iespell',
 				theme_advanced_toolbar_location: 'top',
 				theme_advanced_toolbar_align: 'left',
 				theme_advanced_statusbar_location: 'bottom',
-				theme_advanced_resizing: true,
+				theme_advanced_resizing: false,
 				
 				readonly: readOnly,
 				
