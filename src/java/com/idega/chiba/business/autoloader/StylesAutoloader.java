2888a312159c93c54c06f92300f8dfd6b4242c12
Valdas Žemaitis <valdas@idega.com>
2009-11-03 20:15:00 +0100

    Using Slide API

--- src/java/com/idega/chiba/business/autoloader/StylesAutoloader.java
+++ src/java/com/idega/chiba/business/autoloader/StylesAutoloader.java
@@ -1,14 +1,11 @@
 package com.idega.chiba.business.autoloader;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
-import java.rmi.RemoteException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.apache.commons.httpclient.HttpException;
 import org.chiba.web.IWBundleStarter;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Scope;
@@ -16,12 +13,13 @@ import org.springframework.stereotype.Service;
 
 import com.idega.business.IBOLookup;
 import com.idega.business.IBOLookupException;
+import com.idega.core.file.util.MimeTypeUtil;
 import com.idega.idegaweb.IWApplicationContext;
 import com.idega.idegaweb.IWBundle;
 import com.idega.idegaweb.IWMainApplication;
 import com.idega.idegaweb.IWMainSlideStartedEvent;
 import com.idega.slide.business.IWSlideService;
-import com.idega.slide.util.WebdavExtendedResource;
+import com.idega.util.IOUtil;
 
 /**
  * @author <a href="mailto:anton@idega.com">Anton Makarov</a>
@@ -29,8 +27,8 @@ import com.idega.slide.util.WebdavExtendedResource;
  * 
  * Last modified: $Date: 2008/04/04 14:52:22 $ by $Author: anton $
  */
-@Scope("singleton")
 @Service
+@Scope(BeanDefinition.SCOPE_SINGLETON)
 public class StylesAutoloader implements ApplicationListener {
 
 	private final Logger logger;
@@ -40,52 +38,48 @@ public class StylesAutoloader implements ApplicationListener {
 	}
 
 	public void onApplicationEvent(ApplicationEvent applicationEvent) {	
-		if(applicationEvent instanceof IWMainSlideStartedEvent) {			
+		if (applicationEvent instanceof IWMainSlideStartedEvent) {
+			IWMainApplication iwma = ((IWMainSlideStartedEvent)applicationEvent).getIWMA();
+			IWApplicationContext iwac = iwma.getIWApplicationContext();
 			
+			String path = IWBundleStarter.SLIDE_STYLES_PATH + IWBundleStarter.CHIBA_CSS;
+			InputStream streamToResource = null;
 			try {
-				WebdavExtendedResource webdav_resource = getWebdavExtendedResource(IWBundleStarter.SLIDE_STYLES_PATH + IWBundleStarter.CHIBA_CSS);
-				if (!webdav_resource.exists()) {
-					IWBundle bundle = ((IWMainSlideStartedEvent)applicationEvent).getIWMA().getBundle(IWBundleStarter.BUNDLE_IDENTIFIER);
-					InputStream bis = bundle.getResourceInputStream(IWBundleStarter.CSS_STYLE_PATH + IWBundleStarter.CHIBA_CSS);
-					IWSlideService service_bean = getIWSlideService();
-					
-					service_bean.uploadFileAndCreateFoldersFromStringAsRoot(IWBundleStarter.SLIDE_STYLES_PATH, IWBundleStarter.CHIBA_CSS, bis, "text/css", false);
+				streamToResource = getStreamToResource(iwac, path);
+			} catch (Exception e) {}
+			
+			if (streamToResource == null) {
+				IWBundle bundle = iwma.getBundle(IWBundleStarter.BUNDLE_IDENTIFIER);
+				
+				InputStream streamToBundleResource = null;
+				try {
+					streamToBundleResource = bundle.getResourceInputStream(IWBundleStarter.CSS_STYLE_PATH + IWBundleStarter.CHIBA_CSS);
+					IWSlideService slideService = getIWSlideService(iwac);
+					slideService.uploadFileAndCreateFoldersFromStringAsRoot(IWBundleStarter.SLIDE_STYLES_PATH, IWBundleStarter.CHIBA_CSS, streamToBundleResource,
+							MimeTypeUtil.MIME_TYPE_CSS, Boolean.TRUE);
+				} catch (Exception e) {
+					logger.log(Level.WARNING, "Error uploading to " + path, e);
+				} finally {
+					IOUtil.close(streamToBundleResource);
 				}
-			} catch (IBOLookupException e) {
-				e.printStackTrace();
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (RemoteException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (HttpException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
+			
+			IOUtil.close(streamToResource);
 		}
 	}
 
-	private IWSlideService getIWSlideService() throws IBOLookupException {
+	private IWSlideService getIWSlideService(IWApplicationContext iwac) throws IBOLookupException {
 		try {
-			return (IWSlideService) IBOLookup.getServiceInstance(getIWApplicationContext(), IWSlideService.class);
+			return IBOLookup.getServiceInstance(iwac, IWSlideService.class);
 		} catch (IBOLookupException e) {
 			logger.log(Level.SEVERE, "Error getting IWSlideService");
 			throw e;
 		}
 	}
 
-	private IWApplicationContext getIWApplicationContext() {
-		IWApplicationContext iwac = IWMainApplication
-				.getDefaultIWApplicationContext();
-		return iwac;
-	}
-
-	private WebdavExtendedResource getWebdavExtendedResource(String path) throws HttpException, IOException, RemoteException, IBOLookupException {
-		IWSlideService service = getIWSlideService();
-		return service.getWebdavExtendedResource(path, service.getRootUserCredentials());
+	private InputStream getStreamToResource(IWApplicationContext iwac, String path) throws Exception {
+		IWSlideService service = getIWSlideService(iwac);
+		return service.getInputStream(path);
+		//return service.getWebdavExtendedResource(path, service.getRootUserCredentials());
 	}
 }
