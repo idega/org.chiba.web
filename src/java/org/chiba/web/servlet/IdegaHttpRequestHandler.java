1a3d835430d591f679cc179039b10662f206d00e
Valdas Žemaitis <valdas@idega.com>
2012-09-20 10:17:51 +0300

    Using generics

--- src/java/org/chiba/web/servlet/IdegaHttpRequestHandler.java
+++ src/java/org/chiba/web/servlet/IdegaHttpRequestHandler.java
@@ -18,47 +18,48 @@ import com.idega.util.CoreUtil;
 public class IdegaHttpRequestHandler extends HttpRequestHandler {
 
 	private static final Logger LOGGER = Logger.getLogger(IdegaHttpRequestHandler.class.getName());
-	
+
 	private String xformSessionKey;
-	
+
 	public IdegaHttpRequestHandler(ChibaBean chibaBean) {
 		super(chibaBean);
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
-	protected Map[] parseRequest(HttpServletRequest request) throws FileUploadException, UnsupportedEncodingException {
+	protected Map<?, ?>[] parseRequest(HttpServletRequest request) throws FileUploadException, UnsupportedEncodingException {
 		try {
-			return super.parseRequest(request);
+			Map<?, ?>[] result = super.parseRequest(request);
+			return result;
 		} catch (Exception e) {
 			String error = "Error parsing request";
 			CoreUtil.sendExceptionNotification(error, e);
 			LOGGER.log(Level.WARNING, error, e);
-			
+
 			ChibaUtils.getInstance().markUploadAsFailed(xformSessionKey);
 		}
 		return new Map[4];
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
-	protected Map parseUploadParameter(String name, FileItem item, Map uploads) {
+	protected Map<String, FileItem> parseUploadParameter(String name, FileItem item, @SuppressWarnings("rawtypes") Map uploads) {
 		try {
-			return super.parseUploadParameter(name, item, uploads);
+			@SuppressWarnings("unchecked")
+			Map<String, FileItem> params = super.parseUploadParameter(name, item, uploads);
+			return params;
 		} catch (Exception e) {
 			String error = "Error while parsing upload parameters";
 			CoreUtil.sendExceptionNotification(error, e);
 			LOGGER.log(Level.WARNING, error, e);
-			
+
 			ChibaUtils.getInstance().markUploadAsFailed(xformSessionKey);
 		}
-		return new HashMap();
+		return new HashMap<String, FileItem>();
 	}
-	
+
 	@Override
 	public void setSessionKey(String sessionKey) {
 		super.setSessionKey(sessionKey);
-		
+
 		this.xformSessionKey = sessionKey;
 	}
 }
