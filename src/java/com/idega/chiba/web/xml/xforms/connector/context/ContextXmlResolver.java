b9e483d3d772574fe3a0087d6bf777e429a6c567
Valdas Žemaitis <valdas@idega.com>
2013-08-23 11:10:10 +0300

    Returning null if resolved map is empty

--- src/java/com/idega/chiba/web/xml/xforms/connector/context/ContextXmlResolver.java
+++ src/java/com/idega/chiba/web/xml/xforms/connector/context/ContextXmlResolver.java
@@ -20,6 +20,7 @@ import org.w3c.dom.Document;
 
 import com.idega.chiba.web.xml.xforms.connector.context.beans.ChoiceListData;
 import com.idega.chiba.web.xml.xforms.connector.context.beans.LocalizedEntries;
+import com.idega.util.CoreUtil;
 import com.idega.util.StringUtil;
 import com.idega.util.datastructures.map.MapUtil;
 import com.idega.util.text.Item;
@@ -34,10 +35,10 @@ import com.idega.util.xml.XmlUtil;
 public class ContextXmlResolver extends org.chiba.xml.xforms.connector.context.ContextResolver implements URIResolver {
 
 	private static final Logger LOGGER = Logger.getLogger(ContextXmlResolver.class.getName());
-	
+
 	private static final String faces_exp_part1 = "#{";
 	private static final String faces_exp_part2 = "}";
-	
+
 	/**
 	 * resolves object, which is configured in the faces-config.xml, method value
 	 */
@@ -54,98 +55,98 @@ public class ContextXmlResolver extends org.chiba.xml.xforms.connector.context.C
 			}
         }
     }
-    
+
     protected Document createResponseDocument(Map<Locale, Map<String, String>> localizedItems) throws Exception {
     	if (MapUtil.isEmpty(localizedItems)) {
     		return null;
     	}
-    	
+
     	ChoiceListData choiceListData = new ChoiceListData();
 		for (Entry<Locale, Map<String, String>> localizedItemsEntry : localizedItems.entrySet()) {
 			LocalizedEntries localizedEntries = new LocalizedEntries();
 			localizedEntries.setLang(localizedItemsEntry.getKey().toString());
-			
+
 			for (Entry<String, String> itemEntry : localizedItemsEntry.getValue().entrySet()) {
 				localizedEntries.add(new Item(itemEntry.getKey(), itemEntry.getValue()));
 			}
-			
+
 			choiceListData.add(localizedEntries);
 		}
-    	
+
         return choiceListData.getDocument();
 	}
-    
+
     public Map<Locale, Map<String, String>> resolveItems(String uri) {
     	if (StringUtil.isEmpty(uri)) {
-    		LOGGER.log(Level.WARNING, "No URI was given. Nothing to resolve.");
+    		LOGGER.warning("No URI was given. Nothing to resolve.");
     		return null;
     	}
-    	
-    	setURI(uri);
-    	
-    	String xpath = null;
-		try {
-			xpath = new URI(getURI()).getSchemeSpecificPart();
-		} catch (URISyntaxException e) {
-			LOGGER.log(Level.WARNING, 
-					"Bad URI was given: " + getURI() + 
-					". No items will be resolved.", e);
-		}
-		
-        FacesContext ctx = FacesContext.getCurrentInstance();
-        if (ctx == null) {
-        	LOGGER.log(Level.WARNING, "Unable to get: " + FacesContext.class
-        			+ ". No items will be resolved.");
-        	return null;
-        }
-        
-        ELContext elContext = ctx.getELContext();
-        if (elContext == null) {
-        	LOGGER.log(Level.WARNING, "Unable to get: " + ELContext.class
-        			+ ". No items will be resolved.");
-        	return null;
-        }
-        
-        Application application = ctx.getApplication();
-        if (application == null) {
-        	LOGGER.log(Level.WARNING, "Unable to get: " + Application.class
-        			+ ". No items will be resolved.");
-        	return null;
-        }
-        
-        ExpressionFactory expressionFactory = application.getExpressionFactory();
-        if (expressionFactory == null) {
-        	LOGGER.log(Level.WARNING, "Unable to get: " + 
-        			ExpressionFactory.class + ". No items will be resolved.");
-        	return null;
-        }
-        
-        String expression = new StringBuilder(faces_exp_part1)
-        		.append(xpath)
-        		.append(faces_exp_part2).toString();
-        
-        ValueExpression valueExpression = expressionFactory
-        		.createValueExpression(elContext, expression, Map.class);
-        if (valueExpression == null) {
-        	LOGGER.log(Level.WARNING, "Unable to create: " + 
-        			ValueExpression.class + ". No items will be resolved.");
-        	return null;
-        }
-        
-        Object value = valueExpression.getValue(elContext);
-        if (value == null) {
-        	LOGGER.warning("No value was retrieved from the key: " + xpath);
-        	return null;
-        }
-                
-        if (value instanceof Map) {
-        	return (Map<Locale, Map<String, String>>) value;
-        } else {
-        	LOGGER.warning("Value of wrong type was retrieved from the key: " + 
-        			xpath +	" value class: " + value.getClass().getName() +
-        			". It must be: Map<Locale, Map<String, String>>. " +
-        			"Returning empty XML document");
-        	return null;
-        }
+
+    	try {
+	    	setURI(uri);
+
+	    	String xpath = null;
+			try {
+				xpath = new URI(getURI()).getSchemeSpecificPart();
+			} catch (URISyntaxException e) {
+				LOGGER.log(Level.WARNING, "Bad URI was given: " + getURI() + ". No items will be resolved.", e);
+			}
+
+	        FacesContext ctx = FacesContext.getCurrentInstance();
+	        if (ctx == null) {
+	        	LOGGER.warning("Unable to get: " + FacesContext.class.getName() + ". No items will be resolved for URI: " + uri);
+	        	return null;
+	        }
+
+	        ELContext elContext = ctx.getELContext();
+	        if (elContext == null) {
+	        	LOGGER.warning("Unable to get: " + ELContext.class.getName() + ". No items will be resolved for URI: " + uri);
+	        	return null;
+	        }
+
+	        Application application = ctx.getApplication();
+	        if (application == null) {
+	        	LOGGER.warning("Unable to get: " + Application.class.getName() + ". No items will be resolved for URI: " + uri);
+	        	return null;
+	        }
+
+	        ExpressionFactory expressionFactory = application.getExpressionFactory();
+	        if (expressionFactory == null) {
+	        	LOGGER.warning("Unable to get: " + ExpressionFactory.class.getName() + ". No items will be resolved for URI: " + uri);
+	        	return null;
+	        }
+
+	        String expression = new StringBuilder(faces_exp_part1).append(xpath).append(faces_exp_part2).toString();
+
+	        ValueExpression valueExpression = expressionFactory.createValueExpression(elContext, expression, Map.class);
+	        if (valueExpression == null) {
+	        	LOGGER.warning("Unable to create value expression for '" + expression + "'. No items will be resolved for URI: " + uri);
+	        	return null;
+	        }
+
+	        Object value = valueExpression.getValue(elContext);
+	        if (value == null) {
+	        	LOGGER.warning("No value was retrieved from the XPath: '" + xpath + "' and expression: '" + expression + "'");
+	        	return null;
+	        }
+
+	        if (value instanceof Map) {
+	        	@SuppressWarnings("unchecked")
+				Map<Locale, Map<String, String>> items = (Map<Locale, Map<String, String>>) value;
+	        	if (MapUtil.isEmpty(items))
+	        		return null;
+	        	return items;
+	        } else {
+	        	LOGGER.warning("Value of wrong type was retrieved from the XPath ('" + xpath +	"') and expression ('" + expression +
+	        			"') value class: " + value.getClass().getName() + ". It must be: Map<Locale, Map<String, String>>. " +
+	        			"Returning empty XML document");
+	        	return null;
+	        }
+    	} catch (Exception e) {
+    		String message = "Error resolving value for " + uri;
+    		LOGGER.log(Level.WARNING, message, e);
+    		CoreUtil.sendExceptionNotification(message, e);
+    	}
+    	return null;
     }
 }
